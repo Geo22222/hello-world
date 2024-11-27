51ff8a15fd3af13ea0c849c4a9c886902d49a1fd
Geo22222 <150459299+Geo22222@users.noreply.github.com>
2024-11-27 11:11:26 -0500

    Add files via upload

---
+++ simple_prog.py
@@ -0,0 +1,12 @@
+
+#Set variable c to "cats"
+c = "cats"
+
+#Set variable d to "dog"
+d = "dogs"
+
+#Set varisable s to the sentence to print
+s = f"It is raining {c} and {d}."
+
+#Print the sentence
+print (s)
